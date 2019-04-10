c76a61fd23b78caf3039b4e0960ac89b8fd2c47b
kimgid <kimgidarrah@gmail.com>
2019-04-10 12:28:37 +0100

    first commit
    my first commit

---
+++ polls.php
@@ -0,0 +1,57 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+
+  <title>theLoop</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="customstyle.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <div class="container">
+
+    <nav class="navbar navbar-light bg-light justify-content-between">
+      <a class="navbar-brand"><b><small>the</small>Loop</b></a>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <i class="fab fa-twitter" style="font-size: 1.5em"></i>
+        </li>
+        <li class="nav-item">
+          <i class="fas fa-rss" style="font-size: 1.5em"></i>
+        </li>
+      </ul>
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Email" aria-label="Search">
+        <button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#exampleModal">Subscribe</button>
+        <!-- <button class="btn btn-outline-dark my-2 my-sm-0" type="submit" data-toggle="modal" data-target="#exampleModal">Subscribe</button>-->
+      </form>
+    </nav>
+
+    <!-- <img src='img/bigbencropped2.jpg' id='mainImage'> -->
+
+    <div class="row">
+
+      <iframe width="100%" height="600" frameborder="0" scrolling="no" src="//plot.ly/~kimgid/3.embed?link=false&modebar=false&logo=false"></iframe>
+
+    </div>
+
+</body>
+
+</html>
